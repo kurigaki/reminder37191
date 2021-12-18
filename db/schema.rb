7e89bca21da80d3d7c441726836961fe2b68e489
kurigaki <kenji.kurigaki@gmail.com>
2021-12-19 00:05:35 +0900

    外部キーのエラーの解決

--- db/schema.rb
+++ db/schema.rb
@@ -37,9 +37,11 @@ ActiveRecord::Schema.define(version: 2021_12_18_144029) do
     t.string "title", null: false
     t.text "text", null: false
     t.string "reference", null: false
+    t.bigint "user_id", null: false
     t.integer "genre_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 2021_12_18_144029) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
